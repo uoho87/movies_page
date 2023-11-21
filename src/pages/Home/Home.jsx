a192a67d087cb0bcf711e6ad996d59ecca5a6612
Dave86dev <davidochando1986@gmail.com>
2023-11-21 12:26:31 +0100

    Mapeado de personajes de API externa Rick and Morty con éxito

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,78 @@
 
+// Para poder trabajar con el ciclo de vida del componente, importo los hooks useState y useEffect
+
+import { useState, useEffect } from 'react';
 import "./Home.css"
+import { bringAllCharacters } from '../../services/apiCalls';
 
 export const Home = () => {
 
+    //1 - Primero se observa el valor de los hooks
+    const [characters, setCharacters] = useState([])
+
+    //3 - Después de la zona de renderizado se ejecutan los hooks useEffect y funciones
+
+    // useEffect(()=>{
+    //     //Este useEffect siempre se ejecutará nada más se monte el componente por primera vez
+
+
+    // },[])
+
+    // useEffect(()=>{
+    //     //Este useEffect se ejecutará SIEMPRE que haya una actualización en el componente
+
+    // })
+
+    // useEffect(()=>{
+    //         //Este useEffect se ejecutará cuando se desmonte el componente
+    //     return () => {
+    //         console.log("byee byeeee")
+    //     }
+    // })
+
+    useEffect(()=>{
+        //Este useEffect se ejecutará SIEMPRE que se actualize el hook de estado "characters"
+
+        if(characters.length === 0){
+            //Como al cargarse el componente la longitud de characters es 0... ejecuto la función que los traerá
+            bringAllCharacters()
+                .then(
+                    result => {
+                        console.log(result)
+                        setCharacters(result.data.results)
+                    }
+                )
+                .catch(error => console.log(error))
+        }
+        //Al tener una longitud superior a 0, no entraríamos en el condicional, por lo tanto no llamamos de nuevo a la API
+    },[characters])
+
+
+
+    //2 - El segundo paso es estudiar que hay dentro de la zona de renderizado
     return (
-        <div className="homeDesign">soy Home</div>
+        <div className="homeDesign">
+
+            {
+
+                characters.length > 0 
+
+                ? (<div>
+                    {
+                        characters.map(
+                            character => {
+                                return (
+                                    <div key={character.id}>
+                                        {character.name}
+                                    </div>
+                                )
+                            }
+                        )
+                    }
+                </div>)
+
+                : (<div>Los datos estan viniendo de la API</div>)
+            }
+        </div>
     )
 }
---
+++ src/services/apiCalls.js
@@ -0,0 +1,7 @@
+
+import axios from 'axios'
+
+export const bringAllCharacters = async () => {
+
+    return await axios.get(`https://rickandmortyapi.com/api/character/?page=7`)
+}
